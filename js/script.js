8bee8e51d0a884dfb25525eaf1714f3e2265c323
Maxim Yuzviak <shaylushaytop@gmail.com>
2025-02-28 16:57:59 +0100

    done all

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let age = 13;
+console.log("Im", age, "years old")
+let name = "Maxim";
+console.log("My name is", name)
+let isStudent = true;
+console.log("Is he student? Answer: Its", isStudent)
+let myString = "For every minute you are angry you lose sixty seconds of happiness."
+console.log("My favourite quote is:", myString)
+let myNumber = 2;
+myNumber = myNumber + 10;
+console.log("The result of adding 10+2 is:", myNumber)
+let myNull = null;
+console.log("Null is", myNull)
+let userName = prompt("Write your name");
+alert("Your name is: " +  userName)
+console.log("Тип змінної userName: " + typeof userName);
+let userResult = confirm("Do you really want to leave the page?");
+console.log("Does he want to leave the page? Answer:", userResult)
+alert("Warning! This action may be dangerous")
+confirm("Are you sure you want to continue?")
